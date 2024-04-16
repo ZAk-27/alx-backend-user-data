fc087b188893e19bf4b5f5d197186d364262cfc0
ZAk-27 <touhadi.zakaria@gmail.com>
2024-04-16 14:12:08 +0000

    all up

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""  Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -18,9 +18,27 @@ def status() -> str:
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the num of each objects
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized[/]
+    Returns:
+      - an unauthorized error msg
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden[/]
+    Returns:
+      - a forbidden error msg
+    """
+    abort(403)
